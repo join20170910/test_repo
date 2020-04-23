4370680e253af3cf8330289bc6e49b0904f7f121
john <wyj8574@163.com>
2020-04-23 09:01:04 +0800

    git test

---
+++ git-study/src/test/java/com/bj/gitstudy/GitStudyApplicationTests.java
@@ -0,0 +1,11 @@
+package com.bj.gitstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitStudyApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
